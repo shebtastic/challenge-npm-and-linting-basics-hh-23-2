2d890709c6bb8bdd14fae805c9154fbf27e384a4
Merle Makoben <merle.makoben@neuefische.de>
2022-08-18 11:01:22 +0200

    Add toggle function to image

--- js/_apibutton.js
+++ js/_apibutton.js
@@ -4,8 +4,10 @@ export default function toggleApiButton() {
   apis.forEach(api => {
     const button = api.querySelector('[data-js="button"]');
     const link = api.querySelector('[data-js="link"]');
+    const image = api.querySelector('[data-js="image"]')
     button.addEventListener('click', () => {
       link.classList.toggle('hidden');
+      image.classList.toggle('hidden')
     });
   });
 }
